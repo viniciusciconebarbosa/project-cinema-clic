be9f21f1ba7db29bbf5488b7583da25903829a2f
vinicius <viniciuscicone@gmail.com>
2025-02-24 00:36:18 -0300

    adding first fecth in global context

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -1,12 +1,20 @@
+// app/context/MovieContext.tsx
 "use client";
 
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import axios from 'axios';
+import { trend, findMovie, discover } from '@/constants/url';
+import { Movie } from '@/components/types';
+
 
 interface MovieContextProps {
   genre: string;
   query: string;
+  movies: Movie[];
+  totalPages: number;
   setGenre: (genre: string) => void;
   setQuery: (query: string) => void;
+  fetchMovies: (page?: number) => Promise<void>;
 }
 
 const MovieContext = createContext<MovieContextProps | undefined>(undefined);
@@ -14,9 +22,28 @@ const MovieContext = createContext<MovieContextProps | undefined>(undefined);
 export const MovieProvider = ({ children }: { children: ReactNode }) => {
   const [genre, setGenre] = useState('');
   const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchMovies = async (page: number = 1) => {
+    const url = `${
+      query.length === 0 ? discover : findMovie
+    }&with_genres=${genre}&page=${page}&query=${query}&language=pt-BR&region=BR`;
+
+    const response = await axios.get(url);
+    setMovies(response.data.results);
+    setTotalPages(response.data.total_pages);
+   
+  };
+
+
+  // Busca os filmes sempre que genre, query ou page mudar
+  useEffect(() => {
+    fetchMovies();
+  }, [genre, query]);
 
   return (
-    <MovieContext.Provider value={{ genre, query, setGenre, setQuery }}>
+    <MovieContext.Provider value={{ genre, query, movies, totalPages, setGenre, setQuery, fetchMovies }}>
       {children}
     </MovieContext.Provider>
   );
