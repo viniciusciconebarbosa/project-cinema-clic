7bbca417a6f7fef4772be7df6f07b6cd82c37070
vinicius <viniciuscicone@gmail.com>
2025-02-25 17:37:20 -0300

    update repository

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -53,8 +53,10 @@ export const MovieProvider = ({ children }: { children: ReactNode }) => {
     setTotalPages(response.data.total_pages);
     console.log(url)
   };
-
+ // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
+
+     // eslint-disable-next-line react-hooks/exhaustive-deps
     fetchMovies();
   }, [genre, query]);
 
